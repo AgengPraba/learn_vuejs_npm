9f742e5d5705b91c2b6e83d29499334203e7cea0
Ageng <agengpraba@gmail.com>
2024-02-04 08:11:04 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="app" class="container mt-5">
+    <h1>IDShop</h1>
+    <p class="animated fadeInRight">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione facere
+      voluptatem temporibus voluptates est! Ducimus id saepe fugiat vero
+      molestias numquam earum neque culpa magni cum? Fugit explicabo minus
+      natus. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius
+      rerum minus, pariatur modi reiciendis saepe hic facilis deleniti? Cumque
+      provident quis iusto reiciendis assumenda praesentium perspiciatis
+      corrupti odio saepe rerum.
+    </p>
+    <font-awesome-icon icon="shopping-cart"></font-awesome-icon>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    FontAwesomeIcon,
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
